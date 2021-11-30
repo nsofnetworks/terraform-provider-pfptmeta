2ec42396ac294f956280fa31997aac655549da0b
Hod Alpert <halpert@metanetworks.com>
2021-11-30 21:13:56 +0200

    NSOF-5653 infra: add ID and pattern validators

--- internal/provider/validations.go
+++ internal/provider/validations.go
@@ -4,7 +4,12 @@ import (
 	"fmt"
 	"github.com/hashicorp/go-cty/cty"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"regexp"
+	"strings"
 )
+
+var numericPattern = regexp.MustCompile("^[0-9]{1,30}$")
+var alphabetPattern = regexp.MustCompile("^[a-zA-Z0-9]{1,30}$")
 
 func contains(v string, a []string) bool {
 	for _, i := range a {
@@ -29,5 +34,41 @@ func validateENUM(enum ...string) func(interface{}, cty.Path) diag.Diagnostics {
 				Summary:  fmt.Sprintf("\"%s\" is not one of %+v", inputString, enum),
 			})
 		}
+	}
+}
+
+func validateID(numeric bool, prefixes... string) func(interface{}, cty.Path) diag.Diagnostics{
+	return func(input interface{}, _ cty.Path) diag.Diagnostics {
+		var diags diag.Diagnostics
+		ID := input.(string)
+		parts := strings.Split(ID, "-")
+		if len(parts) != 2 {
+			return diag.Errorf("\"%s\" should be of the form <prefix>-<unique>", ID)
+		}
+		switch {
+		case !contains(parts[0], prefixes):
+			return diag.Errorf("\"%s\" should have a prefix of %v", ID, prefixes)
+		}
+		if numeric {
+			if !numericPattern.MatchString(parts[1]) {
+				return diag.Errorf("\"%s\" should have a numeric suffix", ID)
+			}
+		} else {
+			if !alphabetPattern.MatchString(parts[1]) {
+				return diag.Errorf("\"%s\" should have a alphabet suffix", ID)
+			}
+		}
+		return diags
+	}
+}
+
+func validatePattern(pattern *regexp.Regexp) func(interface{}, cty.Path) diag.Diagnostics{
+	return func(input interface{}, _ cty.Path) diag.Diagnostics {
+		var diags diag.Diagnostics
+		inputString := input.(string)
+		if !pattern.MatchString(inputString) {
+			return diag.Errorf("\"%s\" does not match pattern \"%s\"", inputString, pattern.String())
+		}
+		return diags
 	}
 }
--- internal/provider/validations_test.go
+++ internal/provider/validations_test.go
@@ -1,6 +1,7 @@
 package provider
 
 import (
+	"regexp"
 	"testing"
 )
 
@@ -24,6 +25,85 @@ func TestValidateENUM(t *testing.T) {
 	for name, tc := range cases {
 		t.Run(name, func(t *testing.T) {
 			diags := validateENUM(tc.Enum...)(tc.Input, nil)
+			if diags.HasError() && !tc.ShouldError {
+				t.Errorf("%s failed: %+v", name, diags[0])
+			}
+		})
+	}
+}
+
+func TestValidateID(t *testing.T) {
+	cases := map[string]struct {
+		Input       string
+		numeric     bool
+		prefixes    []string
+		ShouldError bool
+	}{
+		"positive-test1": {
+			Input:       "ne-123",
+			numeric:     true,
+			prefixes:    []string{"ne"},
+			ShouldError: false,
+		},
+		"positive-test2": {
+			Input:       "ne-123",
+			numeric:     true,
+			prefixes:    []string{"ed", "ne"},
+			ShouldError: false,
+		},
+		"negative-test-no-id": {
+			Input:       "12345",
+			prefixes:    []string{"ne"},
+			ShouldError: true,
+		},
+		"negative-test-id-with-no-suffix": {
+			Input:       "ne-",
+			prefixes:    []string{"ne"},
+			ShouldError: true,
+		},
+		"negative-test-non-numeric": {
+			Input:       "ne-abc",
+			numeric:     true,
+			prefixes:    []string{"ne"},
+			ShouldError: true,
+		},
+		"negative-test-non-alphabetic": {
+			Input:       "ne-!@#",
+			numeric:     true,
+			prefixes:    []string{"ne"},
+			ShouldError: true,
+		},
+	}
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			diags := validateID(tc.numeric, tc.prefixes...)(tc.Input, nil)
+			if diags.HasError() && !tc.ShouldError {
+				t.Errorf("%s failed: %+v", name, diags[0])
+			}
+		})
+	}
+}
+
+func TestValidatePattern(t *testing.T) {
+	cases := map[string]struct {
+		Input       string
+		pattern     *regexp.Regexp
+		ShouldError bool
+	}{
+		"positive-test": {
+			Input:       "test123",
+			pattern:    regexp.MustCompile("test[\\d]+"),
+			ShouldError: false,
+		},
+		"negative-test": {
+			Input:       "abcd",
+			pattern:    regexp.MustCompile("[\\d]+"),
+			ShouldError: true,
+		},
+	}
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			diags := validatePattern(tc.pattern)(tc.Input, nil)
 			if diags.HasError() && !tc.ShouldError {
 				t.Errorf("%s failed: %+v", name, diags[0])
 			}
