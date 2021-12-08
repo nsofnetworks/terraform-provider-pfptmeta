c3d03a6d69350ac5a971eee2288ff473f2e853c8
Hod Alpert <halpert@metanetworks.com>
2021-12-08 20:57:48 +0200

    NSOF-5652 provider: remove unsed test step for mapped subnet

--- internal/provider/resource_network_elemets_test.go
+++ internal/provider/resource_network_elemets_test.go
@@ -127,18 +127,6 @@ resource "pfptmeta_network_element" "mapped-subnet" {
 }
 `
 
-const testAccResourceMappedSubnetStep2 = `
-resource "pfptmeta_network_element" "mapped-subnet" {
-  name           = "mapped subnet name"
-  description    = "some details about the mapped subnet"
-  mapped_subnets = ["0.0.0.0/0", "10.20.30.0/24"]
-  tags = {
-    tag_name1 = "tag_value1"
-    tag_name2 = "tag_value2"
-  }
-}
-`
-
 const testAccResourceMappedServiceStep1 = `
 resource "pfptmeta_network_element" "mapped-service" {
   name           = "mapped service name"
