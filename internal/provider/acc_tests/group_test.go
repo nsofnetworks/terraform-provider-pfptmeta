c427ba4209415e6e9c36f99f8c5a57ba576f79e9
Hod Alpert <halpert@metanetworks.com>
2021-12-13 10:16:12 +0200

    NSOF-5653 acc_tests: unite data_source and resource tests of a resource to a single file

--- internal/provider/acc_tests/group_test.go
+++ internal/provider/acc_tests/group_test.go
@@ -48,3 +48,45 @@ resource "pfptmeta_group" "new_group" {
   description = "group description1"
 }
 `
+
+func TestAccDataSourceGroup(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceGroup,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"data.pfptmeta_group.group_by_id", "id", "grp-Y5mxUn6ove1ybrn",
+					),
+					resource.TestCheckResourceAttr(
+						"data.pfptmeta_group.group_by_id", "name", "test-group",
+					),
+					resource.TestCheckResourceAttr(
+						"data.pfptmeta_group.group_by_id", "description", "Some group description",
+					),
+					resource.TestCheckResourceAttr(
+						"data.pfptmeta_group.group_by_name", "id", "grp-Y5mxUn6ove1ybrn",
+					),
+					resource.TestCheckResourceAttr(
+						"data.pfptmeta_group.group_by_name", "name", "test-group",
+					),
+					resource.TestCheckResourceAttr(
+						"data.pfptmeta_group.group_by_name", "description", "Some group description",
+					),
+				),
+			},
+		},
+	})
+}
+
+const testAccDataSourceGroup = `
+data "pfptmeta_group" "group_by_id" {
+  id = "grp-Y5mxUn6ove1ybrn"
+}
+
+data "pfptmeta_group" "group_by_name" {
+  name = "test-group"
+}
+`
--- internal/provider/acc_tests/protocol_group_test.go
+++ internal/provider/acc_tests/protocol_group_test.go
@@ -60,3 +60,57 @@ resource "pfptmeta_protocol_group" "new_protocol" {
   }
 }
 `
+
+func TestAccDataSourceProtocolGroup(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceProtocolGroup,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"data.pfptmeta_protocol_group.HTTP", "id", "pg-NKMzUnJzalxWZKe",
+					),
+					resource.TestCheckResourceAttr(
+						"data.pfptmeta_protocol_group.HTTP", "name", "HTTP",
+					),
+					resource.TestCheckResourceAttr(
+						"data.pfptmeta_protocol_group.HTTP", "protocols.0.from_port", "80",
+					),
+					resource.TestCheckResourceAttr(
+						"data.pfptmeta_protocol_group.HTTP", "protocols.0.to_port", "80",
+					),
+					resource.TestCheckResourceAttr(
+						"data.pfptmeta_protocol_group.HTTP", "protocols.0.proto", "tcp",
+					),
+					resource.TestCheckResourceAttr(
+						"data.pfptmeta_protocol_group.HTTPS", "id", "pg-D6vrsnKbj7q5Kxl",
+					),
+					resource.TestCheckResourceAttr(
+						"data.pfptmeta_protocol_group.HTTPS", "name", "HTTPS",
+					),
+					resource.TestCheckResourceAttr(
+						"data.pfptmeta_protocol_group.HTTPS", "protocols.0.from_port", "443",
+					),
+					resource.TestCheckResourceAttr(
+						"data.pfptmeta_protocol_group.HTTPS", "protocols.0.to_port", "443",
+					),
+					resource.TestCheckResourceAttr(
+						"data.pfptmeta_protocol_group.HTTPS", "protocols.0.proto", "tcp",
+					),
+				),
+			},
+		},
+	})
+}
+
+const testAccDataSourceProtocolGroup = `
+data "pfptmeta_protocol_group" "HTTP" {
+  id = "pg-NKMzUnJzalxWZKe"
+}
+
+data "pfptmeta_protocol_group" "HTTPS" {
+  name = "HTTPS"
+}
+`
