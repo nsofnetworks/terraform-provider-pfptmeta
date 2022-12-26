fb8ef80e24941ea01e9bf49a45f98fbd25b20af8
Hod Alpert <halpert@metanetworks.com>
2022-12-26 14:42:21 +0200

    common: add consts file in common package

---
+++ internal/provider/common/consts.go
@@ -0,0 +1,66 @@
+package common
+
+var Countries = []string{"AD", "AE", "AF", "AG", "AI", "AL", "AM", "AO",
+	"AQ", "AR", "AS", "AT", "AU", "AW", "AX", "AZ", "BA", "BB", "BD", "BE", "BF", "BG", "BH", "BI",
+	"BJ", "BL", "BM", "BN", "BO", "BQ", "BR", "BS", "BT", "BV", "BW", "BY", "BZ", "CA", "CC", "CD",
+	"CF", "CG", "CH", "CI", "CK", "CL", "CM", "CN", "CO", "CR", "CU", "CV", "CW", "CX", "CY", "CZ",
+	"DE", "DJ", "DK", "DM", "DO", "DZ", "EC", "EE", "EG", "EH", "ER", "ES", "ET", "FI", "FJ", "FK",
+	"FM", "FO", "FR", "GA", "GB", "GD", "GE", "GF", "GG", "GH", "GI", "GL", "GM", "GN", "GP", "GQ",
+	"GR", "GS", "GT", "GU", "GW", "GY", "HK", "HM", "HN", "HR", "HT", "HU", "ID", "IE", "IL", "IM",
+	"IN", "IO", "IQ", "IR", "IS", "IT", "JE", "JM", "JO", "JP", "KE", "KG", "KH", "KI", "KM", "KN",
+	"KP", "KR", "KW", "KY", "KZ", "LA", "LB", "LC", "LI", "LK", "LR", "LS", "LT", "LU", "LV", "LY",
+	"MA", "MC", "MD", "ME", "MF", "MG", "MH", "MK", "ML", "MM", "MN", "MO", "MP", "MQ", "MR", "MS",
+	"MT", "MU", "MV", "MW", "MX", "MY", "MZ", "NA", "NC", "NE", "NF", "NG", "NI", "NL", "NO", "NP",
+	"NR", "NU", "NZ", "OM", "PA", "PE", "PF", "PG", "PH", "PK", "PL", "PM", "PN", "PR", "PS", "PT",
+	"PW", "PY", "QA", "RE", "RO", "RS", "RU", "RW", "SA", "SB", "SC", "SD", "SE", "SG", "SH", "SI",
+	"SJ", "SK", "SL", "SM", "SN", "SO", "SR", "SS", "ST", "SV", "SX", "SY", "SZ", "TC", "TD", "TF",
+	"TG", "TH", "TJ", "TK", "TL", "TM", "TN", "TO", "TR", "TT", "TV", "TW", "TZ", "UA", "UG", "UM",
+	"US", "UY", "UZ", "VA", "VC", "VE", "VG", "VI", "VN", "VU", "WF", "WS", "YE", "YT", "ZA", "ZM",
+	"ZW"}
+var ContentTypes = []string{"Abortion", "Abused Drugs",
+	"Adult and Pornography", "Alcohol and Tobacco", "Auctions", "Business and Economy",
+	"Cheating", "Computer and Internet Info", "Computer and Internet Security",
+	"Content Delivery Networks", "Cult and Occult", "Dating", "Dead Sites",
+	"Dynamically Generated Content", "Educational Institutions", "Entertainment and Arts",
+	"Fashion and Beauty", "Financial Services", "Gambling", "Games", "Government", "Gross",
+	"Hacking", "Hate and Racism", "Health and Medicine", "Home and Garden", "Hunting and Fishing",
+	"Illegal", "Image and Video Search", "Individual Stock Advice and Tools", "Internet Portals",
+	"Internet Communications", "Job Search", "Kids", "Legal", "Local Information", "Marijuana",
+	"Military", "Motor Vehicles", "Music", "News and Media", "Nudity", "Online Greeting Cards",
+	"Parked Domains", "Pay to Surf", "Personal sites and Blogs", "Personal Storage",
+	"Philosophy and Political Advocacy", "Questionable", "Real Estate", "Recreation and Hobbies",
+	"Reference and Research", "Religion", "Search Engines", "Sex Education",
+	"Shareware and Freeware", "Shopping", "Social Networking", "Society", "Sports",
+	"Streaming Media", "Swimsuits and Intimate Apparel", "Training and Tools", "Translation",
+	"Travel", "Violence", "Weapons", "Web Advertisements", "Web-based Email", "Web Hosting"}
+
+const (
+	CountriesDoc = "`AD`,`AE`,`AF`,`AG`,`AI`,`AL`,`AM`,`AO`,`AQ`,`AR`,`AS`,`AT`,`AU`,`AW`,`AX`,`AZ`,`BA`,`BB`,`BD`,`BE`,`BF`," +
+		"`BG`,`BH`,`BI`,`BJ`,`BL`,`BM`,`BN`,`BO`,`BQ`,`BR`,`BS`,`BT`,`BV`,`BW`,`BY`,`BZ`,`CA`,`CC`,`CD`,`CF`,`CG`,`CH`," +
+		"`CI`,`CK`,`CL`,`CM`,`CN`,`CO`,`CR`,`CU`,`CV`,`CW`,`CX`,`CY`,`CZ`,`DE`,`DJ`,`DK`,`DM`,`DO`,`DZ`,`EC`,`EE`,`EG`," +
+		"`EH`,`ER`,`ES`,`ET`,`FI`,`FJ`,`FK`,`FM`,`FO`,`FR`,`GA`,`GB`,`GD`,`GE`,`GF`,`GG`,`GH`,`GI`,`GL`,`GM`,`GN`,`GP`," +
+		"`GQ`,`GR`,`GS`,`GT`,`GU`,`GW`,`GY`,`HK`,`HM`,`HN`,`HR`,`HT`,`HU`,`ID`,`IE`,`IL`,`IM`,`IN`,`IO`,`IQ`,`IR`,`IS`," +
+		"`IT`,`JE`,`JM`,`JO`,`JP`,`KE`,`KG`,`KH`,`KI`,`KM`,`KN`,`KP`,`KR`,`KW`,`KY`,`KZ`,`LA`,`LB`,`LC`,`LI`,`LK`,`LR`," +
+		"`LS`,`LT`,`LU`,`LV`,`LY`,`MA`,`MC`,`MD`,`ME`,`MF`,`MG`,`MH`,`MK`,`ML`,`MM`,`MN`,`MO`,`MP`,`MQ`,`MR`,`MS`,`MT`," +
+		"`MU`,`MV`,`MW`,`MX`,`MY`,`MZ`,`NA`,`NC`,`NE`,`NF`,`NG`,`NI`,`NL`,`NO`,`NP`,`NR`,`NU`,`NZ`,`OM`,`PA`,`PE`,`PF`," +
+		"`PG`,`PH`,`PK`,`PL`,`PM`,`PN`,`PR`,`PS`,`PT`,`PW`,`PY`,`QA`,`RE`,`RO`,`RS`,`RU`,`RW`,`SA`,`SB`,`SC`,`SD`,`SE`," +
+		"`SG`,`SH`,`SI`,`SJ`,`SK`,`SL`,`SM`,`SN`,`SO`,`SR`,`SS`,`ST`,`SV`,`SX`,`SY`,`SZ`,`TC`,`TD`,`TF`,`TG`,`TH`,`TJ`," +
+		"`TK`,`TL`,`TM`,`TN`,`TO`,`TR`,`TT`,`TV`,`TW`,`TZ`,`UA`,`UG`,`UM`,`US`,`UY`,`UZ`,`VA`,`VC`,`VE`,`VG`,`VI`,`VN`," +
+		"`VU`,`WF`,`WS`,`YE`,`YT`,`ZA`,`ZM`,`ZW`"
+	ContentTypesDoc = "Abortion`, `Abused Drugs`, " +
+		"`Adult and Pornography`, `Alcohol and Tobacco`, `Auctions`, `Business and Economy`, " +
+		"`Cheating`, `Computer and Internet Info`, `Computer and Internet Security`, " +
+		"`Content Delivery Networks`, `Cult and Occult`, `Dating`, `Dead Sites`, " +
+		"`Dynamically Generated Content`, `Educational Institutions`, `Entertainment and Arts`, " +
+		"`Fashion and Beauty`, `Financial Services`, `Gambling`, `Games`, `Government`, `Gross`, " +
+		"`Hacking`, `Hate and Racism`, `Health and Medicine`, `Home and Garden`, `Hunting and Fishing`, " +
+		"`Illegal`, `Image and Video Search`, `Individual Stock Advice and Tools`, `Internet Portals`, " +
+		"`Internet Communications`, `Job Search`, `Kids`, `Legal`, `Local Information`, `Marijuana`, " +
+		"`Military`, `Motor Vehicles`, `Music`, `News and Media`, `Nudity`, `Online Greeting Cards`, " +
+		"`Parked Domains`, `Pay to Surf`, `Personal sites and Blogs`, `Personal Storage`, " +
+		"`Philosophy and Political Advocacy`, `Questionable`, `Real Estate`, `Recreation and Hobbies`, " +
+		"`Reference and Research`, `Religion`, `Search Engines`, `Sex Education`, " +
+		"`Shareware and Freeware`, `Shopping`, `Social Networking`, `Society`, `Sports`, " +
+		"`Streaming Media`, `Swimsuits and Intimate Apparel`, `Training and Tools`, `Translation`, " +
+		"`Travel`, `Violence`, `Weapons`, `Web Advertisements`, `Web-based Email`, `Web Hosting`"
+)
--- internal/provider/threat_category/common.go
+++ internal/provider/threat_category/common.go
@@ -5,6 +5,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/nsofnetworks/terraform-provider-pfptmeta/internal/client"
+	"github.com/nsofnetworks/terraform-provider-pfptmeta/internal/provider/common"
 	"log"
 	"net/http"
 )
@@ -29,18 +30,7 @@ const (
 		"When the threshold is crossed, a rule violation is triggered. " +
 		"ENUM: `LOW`, `MEDIUM`, `HIGH`."
 	countriesDesc = "A list of countries to which access should be restricted. Each country should be represented by a Alpha-2 code (ISO-3166). " +
-		"Enum: `AD`,`AE`,`AF`,`AG`,`AI`,`AL`,`AM`,`AO`,`AQ`,`AR`,`AS`,`AT`,`AU`,`AW`,`AX`,`AZ`,`BA`,`BB`,`BD`,`BE`,`BF`," +
-		"`BG`,`BH`,`BI`,`BJ`,`BL`,`BM`,`BN`,`BO`,`BQ`,`BR`,`BS`,`BT`,`BV`,`BW`,`BY`,`BZ`,`CA`,`CC`,`CD`,`CF`,`CG`,`CH`," +
-		"`CI`,`CK`,`CL`,`CM`,`CN`,`CO`,`CR`,`CU`,`CV`,`CW`,`CX`,`CY`,`CZ`,`DE`,`DJ`,`DK`,`DM`,`DO`,`DZ`,`EC`,`EE`,`EG`," +
-		"`EH`,`ER`,`ES`,`ET`,`FI`,`FJ`,`FK`,`FM`,`FO`,`FR`,`GA`,`GB`,`GD`,`GE`,`GF`,`GG`,`GH`,`GI`,`GL`,`GM`,`GN`,`GP`," +
-		"`GQ`,`GR`,`GS`,`GT`,`GU`,`GW`,`GY`,`HK`,`HM`,`HN`,`HR`,`HT`,`HU`,`ID`,`IE`,`IL`,`IM`,`IN`,`IO`,`IQ`,`IR`,`IS`," +
-		"`IT`,`JE`,`JM`,`JO`,`JP`,`KE`,`KG`,`KH`,`KI`,`KM`,`KN`,`KP`,`KR`,`KW`,`KY`,`KZ`,`LA`,`LB`,`LC`,`LI`,`LK`,`LR`," +
-		"`LS`,`LT`,`LU`,`LV`,`LY`,`MA`,`MC`,`MD`,`ME`,`MF`,`MG`,`MH`,`MK`,`ML`,`MM`,`MN`,`MO`,`MP`,`MQ`,`MR`,`MS`,`MT`," +
-		"`MU`,`MV`,`MW`,`MX`,`MY`,`MZ`,`NA`,`NC`,`NE`,`NF`,`NG`,`NI`,`NL`,`NO`,`NP`,`NR`,`NU`,`NZ`,`OM`,`PA`,`PE`,`PF`," +
-		"`PG`,`PH`,`PK`,`PL`,`PM`,`PN`,`PR`,`PS`,`PT`,`PW`,`PY`,`QA`,`RE`,`RO`,`RS`,`RU`,`RW`,`SA`,`SB`,`SC`,`SD`,`SE`," +
-		"`SG`,`SH`,`SI`,`SJ`,`SK`,`SL`,`SM`,`SN`,`SO`,`SR`,`SS`,`ST`,`SV`,`SX`,`SY`,`SZ`,`TC`,`TD`,`TF`,`TG`,`TH`,`TJ`," +
-		"`TK`,`TL`,`TM`,`TN`,`TO`,`TR`,`TT`,`TV`,`TW`,`TZ`,`UA`,`UG`,`UM`,`US`,`UY`,`UZ`,`VA`,`VC`,`VE`,`VG`,`VI`,`VN`," +
-		"`VU`,`WF`,`WS`,`YE`,`YT`,`ZA`,`ZM`,`ZW`"
+		"Enum: " + common.CountriesDoc
 	typesDesc = "A list of predefined threat types to protect against. " +
 		"Enum:`Abused TLD`,`Bitcoin Related`,`Blackhole`,`Botnets`,`Brute Forcer`,`Chat Server`,`CnC`," +
 		"`Compromised`,`DDoS Target`,`Drop`,`DynDNS`,`EXE Source`,`Fake AV`,`IP Check`,`Keyloggers and Monitoring`," +
