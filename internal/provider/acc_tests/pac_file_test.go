515216e5a7155363230ccb8d62463a8bda3a3858
Nadav Forer <nadav@metanetworks.com>
2023-07-25 15:06:33 +0300

    NSOF-6622 pac_file_test.go: coding: updated names of resources and tests
    these updates affect no logic changes but are nice to have as a separation between
    the current tests and those that will be added in the next commits, where PAC files
    are differentiated by types. the renaming of the resources themselves also serves to
    allow for tests to pass even if previous ones failed, due to hanging PAC files of same
    name in the org itself

--- internal/provider/acc_tests/pac_file_test.go
+++ internal/provider/acc_tests/pac_file_test.go
@@ -7,10 +7,10 @@ import (
 )
 
 const (
-	pacFileStep1 = `
+	byoPacNew = `
 resource "pfptmeta_pac_file" "pac" {
-  name         = "pac file"
-  description  = "pac file description"
+  name         = "test byo pac file"
+  description  = "test byo pac file description"
   apply_to_org = true
   priority     = 15
   content      = <<EOF
@@ -20,10 +20,10 @@ function FindProxyForURL(url, host) {
 EOF
 }
 `
-	pacFileStep2 = `
+	byoPacUpdated = `
 resource "pfptmeta_pac_file" "pac" {
-  name         = "pac file 1"
-  description  = "pac file description 1"
+  name         = "test byo pac file 1"
+  description  = "test byo pac file description 1"
   apply_to_org = false
   priority     = 20
   content      = <<EOF
@@ -33,11 +33,10 @@ function FindProxyForURL(url, host) {
 EOF
 }
 `
-	pacFileStepDataSource = `
-
-resource "pfptmeta_pac_file" "pac" {
-  name         = "pac file"
-  description  = "pac file description"
+	byoPacFileDataSource = `
+resource "pfptmeta_pac_file" "pac_data_source" {
+  name         = "test byo pac file data source"
+  description  = "test byo pac file data source description"
   apply_to_org = true
   priority     = 15
   content      = <<EOF
@@ -47,35 +46,35 @@ function FindProxyForURL(url, host) {
 EOF
 }
 
-data "pfptmeta_pac_file" "pac" {
-  id = pfptmeta_pac_file.pac.id
+data "pfptmeta_pac_file" "pac_data_source" {
+  id = pfptmeta_pac_file.pac_data_source.id
 }
 `
 )
 
-func TestAccResourcePacFile(t *testing.T) {
+func TestAccResourceBYOPacFile(t *testing.T) {
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:          func() { testAccPreCheck(t) },
 		ProviderFactories: providerFactories,
 		CheckDestroy:      validateResourceDestroyed("pac_file", "v1/pac_files"),
 		Steps: []resource.TestStep{
 			{
-				Config: pacFileStep1,
+				Config: byoPacNew,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestMatchResourceAttr("pfptmeta_pac_file.pac", "id", regexp.MustCompile("^pf-.+$")),
-					resource.TestCheckResourceAttr("pfptmeta_pac_file.pac", "name", "pac file"),
-					resource.TestCheckResourceAttr("pfptmeta_pac_file.pac", "description", "pac file description"),
+					resource.TestCheckResourceAttr("pfptmeta_pac_file.pac", "name", "test byo pac file"),
+					resource.TestCheckResourceAttr("pfptmeta_pac_file.pac", "description", "test byo pac file description"),
 					resource.TestCheckResourceAttr("pfptmeta_pac_file.pac", "apply_to_org", "true"),
 					resource.TestCheckResourceAttr("pfptmeta_pac_file.pac", "priority", "15"),
 					resource.TestCheckResourceAttr("pfptmeta_pac_file.pac", "content", "function FindProxyForURL(url, host) {\n  return \"PROXY 127.0.0.1:43443321\";\n}\n"),
 				),
 			},
 			{
-				Config: pacFileStep2,
+				Config: byoPacUpdated,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestMatchResourceAttr("pfptmeta_pac_file.pac", "id", regexp.MustCompile("^pf-.+$")),
-					resource.TestCheckResourceAttr("pfptmeta_pac_file.pac", "name", "pac file 1"),
-					resource.TestCheckResourceAttr("pfptmeta_pac_file.pac", "description", "pac file description 1"),
+					resource.TestCheckResourceAttr("pfptmeta_pac_file.pac", "name", "test byo pac file 1"),
+					resource.TestCheckResourceAttr("pfptmeta_pac_file.pac", "description", "test byo pac file description 1"),
 					resource.TestCheckResourceAttr("pfptmeta_pac_file.pac", "apply_to_org", "false"),
 					resource.TestCheckResourceAttr("pfptmeta_pac_file.pac", "priority", "20"),
 					resource.TestCheckResourceAttr("pfptmeta_pac_file.pac", "content", "function FindProxyForURL(url, host) {\n  return \"PROXY 127.0.0.1:43443\";\n}\n"),
@@ -85,22 +84,23 @@ func TestAccResourcePacFile(t *testing.T) {
 	})
 }
 
-func TestAccDataSourcePacFile(t *testing.T) {
+func TestAccDataSourceBYOPacFile(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:          func() { testAccPreCheck(t) },
 		ProviderFactories: providerFactories,
 		Steps: []resource.TestStep{
 			{
-				Config: pacFileStepDataSource,
+				Config: byoPacFileDataSource,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestMatchResourceAttr("data.pfptmeta_pac_file.pac", "id", regexp.MustCompile("^pf-.+$")),
-					resource.TestCheckResourceAttr("data.pfptmeta_pac_file.pac", "name", "pac file"),
-					resource.TestCheckResourceAttr("data.pfptmeta_pac_file.pac", "description", "pac file description"),
-					resource.TestCheckResourceAttr("data.pfptmeta_pac_file.pac", "apply_to_org", "true"),
-					resource.TestCheckResourceAttr("data.pfptmeta_pac_file.pac", "priority", "15"),
-					resource.TestCheckResourceAttr("data.pfptmeta_pac_file.pac", "content", "function FindProxyForURL(url, host) {\n  return \"PROXY 127.0.0.1:43443\";\n}\n"),
+					resource.TestMatchResourceAttr("data.pfptmeta_pac_file.pac_data_source", "id", regexp.MustCompile("^pf-.+$")),
+					resource.TestCheckResourceAttr("data.pfptmeta_pac_file.pac_data_source", "name", "test byo pac file data source"),
+					resource.TestCheckResourceAttr("data.pfptmeta_pac_file.pac_data_source", "description", "test byo pac file data source description"),
+					resource.TestCheckResourceAttr("data.pfptmeta_pac_file.pac_data_source", "apply_to_org", "true"),
+					resource.TestCheckResourceAttr("data.pfptmeta_pac_file.pac_data_source", "priority", "15"),
+					resource.TestCheckResourceAttr("data.pfptmeta_pac_file.pac_data_source", "content", "function FindProxyForURL(url, host) {\n  return \"PROXY 127.0.0.1:43443\";\n}\n"),
 				),
 			},
 		},
 	})
 }
+
