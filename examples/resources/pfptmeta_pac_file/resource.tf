b6c2bed0901b4418a115cacfc46e1b6c38dcfd80
Nadav Forer <nadav@metanetworks.com>
2023-07-25 15:11:36 +0300

    NSOF-6622 pac file docs and examples: updated to reflect managed PAC

--- examples/resources/pfptmeta_pac_file/resource.tf
+++ examples/resources/pfptmeta_pac_file/resource.tf
@@ -1,8 +1,22 @@
-resource "pfptmeta_pac_file" "pac" {
+resource "pfptmeta_pac_file" "managed_pac" {
   name         = "pac file"
   description  = "pac file description"
   apply_to_org = true
   priority     = 15
+  type         = "managed"
+  managed_content = {
+    domains     = ["apple.co"]
+    cloud_apps  = ["ca-zcR3wRh1mQLyD", "ca-YbU9LeY4bCAuV"]
+    ip_networks = ["ipn-wRvGyyK1mPEop"]
+  }
+}
+
+resource "pfptmeta_pac_file" "byo_pac" {
+  name         = "pac file"
+  description  = "pac file description"
+  apply_to_org = true
+  priority     = 15
+  type         = "bring_your_own"
   content      = <<EOF
 function FindProxyForURL(url, host) {
 // Don't proxy specific hostnames
@@ -15,10 +29,11 @@ return "PROXY 127.0.0.1:43443";
 EOF
 }
 
-resource "pfptmeta_pac_file" "pac_with_file_path" {
+resource "pfptmeta_pac_file" "byo_pac_with_file_path" {
   name         = "pac file"
   description  = "pac file description"
   apply_to_org = true
   priority     = 15
+  type         = "bring_your_own"
   content      = file("path/to/file")
 }
