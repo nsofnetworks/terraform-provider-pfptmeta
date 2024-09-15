22fea0502f895b21705954e0f572ade2d2639dc9
Gadi Fleischer <gfleischer@metanetworks.com>
2024-09-15 10:46:12 +0300

    Example for debugging
    go the the directory of the file example you want to add
    then export the printed `TF_REATTACH_PROVIDERS` as env variable in a diff terminal
    This will compile the provider in debug mode and will allow you to execute terraform command with it.
    then:
    terraform init
    terraform plan
    terraform apply
    
    dont forget to unset TF_REATTACH_PROVIDERS when done

--- examples/resources/pfptmeta_device/resource.tf
+++ examples/resources/pfptmeta_device/resource.tf
@@ -1,7 +1,34 @@
-resource "pfptmeta_device" "device" {
-  name        = "my device"
-  description = "some details about the device"
-  owner_id    = "usr-abc123"
+# resource "pfptmeta_device" "device" {
+#   name        = "my device"
+#   description = "some details about the device"
+#   owner_id    = "usr-abc123"
+#   tags = {
+#     tag_name1 = "tag_value1"
+#     tag_name2 = "tag_value2"
+#   }
+# }
+
+
+terraform {
+  required_providers {
+    pfptmeta = {
+      source  = "nsofnetworks/pfptmeta"
+      version = "0.1.49"
+    }
+  }
+}
+
+provider "pfptmeta" {
+  api_key       = "key-xe35s61AkYyDMrj"
+  api_secret    = "cb8f94a3ca32fe8a1d2eb254b44d2a2c870b9588845a5206be18aa465f89e64ef6996c1e7c5347ee"
+  org_shortname = "playground"
+  realm         = "us"
+}
+
+resource "pfptmeta_network_element" "mapped_service2" {
+  name           = "example terraform"
+  description    = "some details about the mapped service"
+  mapped_service = "mapped.service.com"
   tags = {
     tag_name1 = "tag_value1"
     tag_name2 = "tag_value2"
